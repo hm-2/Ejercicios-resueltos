70b5a5c1a4ee00b1a8cf99e4e4446c1893a8f8a9
hm-2 <40736565+hm-2@users.noreply.github.com>
2019-06-17 20:11:31 -0300

    Add files via upload

---
+++ FindTheNumber.js
@@ -0,0 +1,13 @@
+//Find the number!
+//Given an unsorted array of n elements, find if the element k is present in the array or not.
+
+function oddNumbers(l,r) {
+	while(r>=l){
+		if(l%2!==0) {
+			console.log(l);
+		}
+		l++
+	}
+}
+
+
---
+++ oddNumbers.js
@@ -0,0 +1,13 @@
+//Odd Numbers!
+//Given two integers l and r print all the odd number between l and r (l and r inclusive).
+
+function findNumber(arr,k) {
+	for(var i=0;i<arr.length;i++){
+		if(arr[i]==k){
+			return "YES"
+		}
+	}
+	return "NO"
+}
+
+
